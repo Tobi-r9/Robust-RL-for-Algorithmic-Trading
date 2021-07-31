8616eb5e23a88a6bbd61aa992826cc1dfb123286
Tobi-r9 <tobihoepp@gmail.com>
2021-07-31 22:21:32 +0200

    add two output layers

--- models/neural_network.py
+++ models/neural_network.py
@@ -9,42 +9,55 @@ class NeuralNetwork(tfk.Model):
         self, 
         hidden_nodes,
         dim, 
+        n_actions,
+        n_lstm_cells=128,
         dropout=None, 
         batch_norm=False,
         l2_reg=0,
+        batch_size=32,
         last_act_fct='linear'
         ):
 
         super(NeuralNetwork, self).__init__()
         self.hidden_nodes = hidden_nodes
         self.n_layers = len(hidden_nodes)
+        self.batch_size = batch_size
+        self.num_actions = n_actions
+        self.n_lstm_cells = n_lstm_cells
         self.dim = dim
-        self.layers_list = []
-
+        self.dense_layers = []
+        self.input_layer = tfkl.InputLayer(
+            input_shape=self.dim, 
+            batch_size=self.batch_size
+            )
         for k in range(0, self.n_layers):
-            if k == 0:
-                #self.layers_list.append(tfkl.Flatten(input_shape=self.dim))
-                self.layers_list.append(tfkl.Dense(self.hidden_nodes[k], activation='relu', input_shape=self.dim))
-                if batch_norm:
-                    self.layers_list.append(tfkl.BatchNormalization())
-                if dropout != None:
-                    self.layers_list.append(tfkl.Dropout(dropout, name='dropout_layer'))
-            elif k == self.n_layers - 2:
-                self.layers_list.append(tfkl.LSTM(self.hidden_nodes[k], activation='tanh', recurrent_activation='sigmoid', return_sequences=False))
-            elif k == self.n_layers - 1:
-                self.layers_list.append(tfkl.Dense(self.hidden_nodes[k], kernel_regularizer=tfk.regularizers.l2(l2_reg), activation=last_act_fct))
-            else:
-                self.layers_list.append(tfkl.Dense(self.hidden_nodes[k], activation='relu'))
-                if batch_norm:
-                    self.layers_list.append(tfkl.BatchNormalization())
-                if dropout != None:
-                    self.layers_list.append(tfkl.Dropout(dropout, name='dropout_layer'))
+            self.dense_layers.append(
+                tfkl.Dense(
+                self.hidden_nodes[k], 
+                activation='relu')
+                )
+        self.lstm_layer = tfkl.LSTM(
+            self.n_lstm_cells, 
+            activation='tanh', 
+            recurrent_activation='sigmoid',
+            return_sequences=False
+            )
+        self.policy_layer = tfkl.Dense(
+            self.n_actions, 
+            kernel_regularizer=tfk.regularizers.l2(l2_reg), 
+            activation='softmax'
+            )
+        self.value_layer = tfkl.Dense(
+            1, 
+            kernel_regularizer=tfk.regularizers.l2(l2_reg), 
+            activation='linear'
+            )
 
-        self.model = tfkm.Sequential(self.layers_list)
-
-    def call(self, x, training=False):
+    def call(self, input):
+        x = self.input_layer(input)
         for layer in self.layers_list:
             x = layer(x)
-            if 'dropout_layer' in layer.name:
-                x = layer(x, training=training)
-        return x
+        x = self.lstm_layer(x)
+        action = self.policy_layer(x)
+        value = self.value_layer(x)
+        return action, value
